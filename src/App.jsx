57b330caad4590d9d7138820038a6c5e5a1fe363
Md Taufique <taufiquefuturetech@gmail.com>
2024-10-18 17:09:30 +0530

    fakeAuthentication is implemented

--- src/App.jsx
+++ src/App.jsx
@@ -11,34 +11,44 @@ import CountriesList from "./components/CountriesList";
 import City from "./components/City";
 import Form from "./components/Form";
 import { CitiesProvider } from "./contexts/citiesContexts";
-
+import { AuthProvider } from "./contexts/fakeAuthContext";
+import ProtectedRoutes from "./pages/ProtectedRoutes";
 function App() {
   // console.log(cities)
 
   return (
     <div>
-      <CitiesProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route index element={<Homepage />} />
-            <Route path="/product" element={<Product />} />
-            <Route path="/pricing" element={<Pricing />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/app" element={<AppLayout />}>
-              {/* <Route index element={<CityList cities={cities} isLoading={isLoading} />} /> */}
-              <Route index element={<Navigate replace to="cities" />} />{" "}
-              {/* aisa krne se defdault me ab cities bhi route me rahge means url me */}
-              {/* replace lagane se hum back jayenge sahi se ye jaruri he  */}
-              {/*   //ye default he index path k amtalab he outlet use kya he sidebar me wo iska dikhayega childs ka */}
-              <Route path="cities" element={<CityList />} />
-              <Route path="cities/:id" element={<City />} />
-              <Route path="countries" element={<CountriesList />} />
-              <Route path="form" element={<Form />} />
-            </Route>
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </CitiesProvider>
+      <AuthProvider>
+        <CitiesProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route index element={<Homepage />} />
+              <Route path="/product" element={<Product />} />
+              <Route path="/pricing" element={<Pricing />} />
+              <Route path="/login" element={<Login />} />
+              <Route
+                path="/app"
+                element={
+                  <ProtectedRoutes>
+                    <AppLayout />
+                  </ProtectedRoutes>
+                }
+              >
+                {/* <Route index element={<CityList cities={cities} isLoading={isLoading} />} /> */}
+                <Route index element={<Navigate replace to="cities" />} />{" "}
+                {/* aisa krne se defdault me ab cities bhi route me rahge means url me */}
+                {/* replace lagane se hum back jayenge sahi se ye jaruri he  */}
+                {/*   //ye default he index path k amtalab he outlet use kya he sidebar me wo iska dikhayega childs ka */}
+                <Route path="cities" element={<CityList />} />
+                <Route path="cities/:id" element={<City />} />
+                <Route path="countries" element={<CountriesList />} />
+                <Route path="form" element={<Form />} />
+              </Route>
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </CitiesProvider>
+      </AuthProvider>
     </div>
   );
 }
